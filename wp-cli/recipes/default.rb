65884933795c404c017ae8798f73819096611c58
EC2 Default User <ec2-user@ip-172-31-35-106.us-west-2.compute.internal>
2014-04-10 06:35:46 +0000

    wp-cli recipe

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -1,12 +1,12 @@
 wpdir = "/usr/bin"
 
-pacgage "mysql" do
+package "mysql" do
 	action :insatall
 end
 
 execute "wp-cli install" do
-	command "curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
-	cwd '/home/ec2-user'
-	action :run
-	not_if { ::File.exists?("#{wpdir}/wp")}
+  command "curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
+  cwd '/home/ec2-user'
+  action :run
+  not_if { ::File.exists?("#{wpdir}/wp")}
 end
